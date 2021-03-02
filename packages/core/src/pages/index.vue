f3654006a64cfa643456c720a8058acb96b649f2
falstack <icesilt@outlook.com>
2021-03-02 16:59:46 +0800

    feat: add element-plus & ant-design-vue

--- packages/core/src/pages/index.vue
+++ packages/core/src/pages/index.vue
@@ -3,11 +3,20 @@
     <div id="_uploader">vue-01 loading...</div>
     <br />
     <div id="_other">vue-02 loading...</div>
+    <Button>ant-design-vue 按钮</Button>
+    <ElButton>element-plus 按钮</ElButton>
   </div>
 </template>
 
 <script>
+import { Button } from 'ant-design-vue'
+import { ElButton } from 'element-plus'
+
 export default {
-  name: 'HomePage'
+  name: 'HomePage',
+  components: {
+    Button,
+    ElButton
+  }
 }
 </script>
--- packages/core/vite.config.js
+++ packages/core/vite.config.js
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
+import styleImport from 'vite-plugin-style-import'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   css: {
     preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+        modifyVars: {
+          'primary-color': '#1DA57A',
+          'link-color': '#1DA57A',
+          'border-radius-base': '2px'
+        }
+      },
       scss: {
         // example : additionalData: `@import "./src/design/styles/variables";`
         // dont need include file extend .scss
@@ -18,7 +27,29 @@ export default defineConfig({
       '@': resolve(__dirname, './src')
     }
   },
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    styleImport({
+      libs: [
+        {
+          libraryName: 'ant-design-vue',
+          esModule: true,
+          resolveStyle: (name) => {
+            return `ant-design-vue/es/${name}/style/index`
+          }
+        },
+        {
+          libraryName: 'element-plus',
+          resolveStyle: (name) => {
+            return `element-plus/lib/theme-chalk/${name}.css`
+          },
+          resolveComponent: (name) => {
+            return `element-plus/lib/${name}`
+          }
+        }
+      ]
+    })
+  ],
   ssgOptions: {
     entry: 'src/main.js'
   }
