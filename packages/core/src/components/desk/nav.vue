4ff204ef198807ba5861cdb9a45d749760281cd9
falstack <icesilt@outlook.com>
2021-03-18 20:10:40 +0800

    feat: add desk size show

--- packages/core/src/components/desk/nav.vue
+++ packages/core/src/components/desk/nav.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="desk-nav">
-    <desk-uploader />
-    <el-button v-if="folderId === -1" size="small" icon="el-icon-folder-add" @click="createFolder">
-      新建文件夹
-    </el-button>
+    <div>
+      <desk-uploader />
+      &nbsp;&nbsp;
+      <el-button v-if="folderId === -1" size="small" icon="el-icon-folder-add" @click="createFolder">
+        新建文件夹
+      </el-button>
+    </div>
+    <div>
+      <desk-size />
+    </div>
   </div>
 </template>
 
 <script>
 import DeskUploader from './uploader.vue'
+import DeskSize from './size.vue'
 
 export default {
   name: 'DeskNav',
   components: {
+    DeskSize,
     DeskUploader
   },
   props: {},
@@ -58,12 +66,15 @@ export default {
 .desk-nav {
   display: flex;
   flex-direction: row;
-  justify-content: flex-start;
+  justify-content: space-between;
   align-items: center;
   padding: 14px 10px;
 
   > * {
-    margin-right: 10px;
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: center;
   }
 }
 </style>
---
+++ packages/core/src/components/desk/size.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="desk-size">
+    <el-progress :text-inside="true" :stroke-width="20" :percentage="percent" status="exception">
+      <span v-text="size" />
+    </el-progress>
+  </div>
+</template>
+
+<script>
+import filesize from 'filesize'
+
+export default {
+  name: 'DeskSize',
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {
+    user() {
+      return this.$store.state.user
+    },
+    percent() {
+      return ((this.user.desk_use_space / this.user.desk_max_space) * 100) | 0
+    },
+    size() {
+      return `${filesize(this.user.desk_use_space)} / ${filesize(this.user.desk_max_space)}`
+    }
+  },
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {}
+}
+</script>
+
+<style lang="scss">
+.desk-size {
+  width: 140px;
+}
+</style>
